aeb2e31f9a4365e4b3cd70a3f8fbd2776402b931
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2022-03-23 21:55:59 +1100

    added check to determine whether user is at least 18 years old

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -28,6 +28,25 @@ usersRouter.post(
     }
 
     // check whether the person is at least 18 years old
+    // reference: https://stackoverflow.com/questions/4060004/calculate-age-given-the-birth-date-in-the-format-yyyymmdd/7091965#7091965
+    const today = new Date();
+    const convertedBirthdate = new Date(birthdate);
+    const monthsDifference = today.getMonth() - convertedBirthdate.getMonth();
+    const daysDifference = today.getDate() - convertedBirthdate.getDate();
+    // if today's month is smaller, this means birth month has not passed yet
+    // if today's the birth month, check whether today's date is smaller
+    const isBirthdateNotPassed =
+      monthsDifference < 0 || (monthsDifference === 0 && daysDifference < 0);
+    // if birthdate not passed yet, decrement age
+    const age =
+      today.getFullYear() -
+      convertedBirthdate.getFullYear() -
+      (isBirthdateNotPassed ? 1 : 0);
+    if (age < 18) {
+      return response.status(400).json({
+        error: "user must be at least 18 years old to register",
+      });
+    }
 
     // 2 ^ 10 hashing iterations (Reference: https://stackoverflow.com/questions/46693430/what-are-salt-rounds-and-how-are-salts-stored-in-bcrypt)
     const saltRounds = 10;
