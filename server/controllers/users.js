2b898a76a340153a62b072ec2867a7cb3effd875
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2022-03-23 20:57:52 +1100

    added an additional check when a user is created
    this is to ensure that one email address cannot be used to register multiple users

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -5,6 +5,15 @@ const User = require("../models/user");
 // create a user when a user is registering to the authentication system
 usersRouter.post("/", async (request, response, next) => {
   const { name, email, password } = request.body;
+
+  // check for duplicate email address
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return response.status(400).json({
+      error: "email address has already been taken",
+    });
+  }
+
   // 2 ^ 10 hashing iterations (Reference: https://stackoverflow.com/questions/46693430/what-are-salt-rounds-and-how-are-salts-stored-in-bcrypt)
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
