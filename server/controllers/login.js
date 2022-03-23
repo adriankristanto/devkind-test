5a6344f1f37bf9d6470abbed1675ca40e548277e
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2022-03-23 23:22:52 +1100

    added try catch for the login controller

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -19,29 +19,35 @@ loginRouter.post(
 
     const { email, password } = request.body;
 
-    // since user is not converted to JSON, it still has access to _id
-    // only when it is returned to the client, e.g. response.status(201).json(savedUser);
-    // the _id field will be removed
-    const user = await User.findOne({ email });
-    const passwordCorrect =
-      user === null ? false : await bcrypt.compare(password, user.passwordHash);
+    try {
+      // since user is not converted to JSON, it still has access to _id
+      // only when it is returned to the client, e.g. response.status(201).json(savedUser);
+      // the _id field will be removed
+      const user = await User.findOne({ email });
+      const passwordCorrect =
+        user === null
+          ? false
+          : await bcrypt.compare(password, user.passwordHash);
 
-    if (!(user && passwordCorrect)) {
-      return response.status(401).json({
-        error: "invalid email or password",
-      });
+      if (!(user && passwordCorrect)) {
+        return response.status(401).json({
+          error: "invalid email or password",
+        });
+      }
+
+      // Claims are statements about an entity (typically, the user) and additional data
+      // reference: https://jwt.io/introduction
+      const userForToken = {
+        email: user.email,
+        id: user._id,
+      };
+
+      const token = jwt.sign(userForToken, config.SECRET, { expiresIn: "1h" });
+
+      response.status(200).send({ token, email: user.email, name: user.name });
+    } catch (exception) {
+      next(exception);
     }
-
-    // Claims are statements about an entity (typically, the user) and additional data
-    // reference: https://jwt.io/introduction
-    const userForToken = {
-      email: user.email,
-      id: user._id,
-    };
-
-    const token = jwt.sign(userForToken, config.SECRET, { expiresIn: "1h" });
-
-    response.status(200).send({ token, email: user.email, name: user.name });
   }
 );
 
