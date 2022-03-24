23f86809bc16f48b605b272b667999f1905cf09e
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2022-03-24 13:07:07 +1100

    added styling to the login component with tailwind

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { Formik, Field, Form } from "formik";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function Login() {
   const { login, setCurrentUser, setToken } = useAuth();
@@ -23,34 +23,52 @@ export default function Login() {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <Formik
-        initialValues={{
-          email: "",
-          password: "",
-        }}
-        onSubmit={handleSubmit}
-      >
-        <Form>
-          <label htmlFor="email">Email</label>
-          <Field
-            id="email"
-            name="email"
-            placeholder="Enter your email here..."
-            type="email"
-          />
+    <div className="container max-w-sm flex-1 flex flex-col items-center justify-center">
+      <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+        <h1 className="mb-8 text-3xl text-center">Login</h1>
+        <Formik
+          initialValues={{
+            email: "",
+            password: "",
+          }}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <label htmlFor="email">Email</label>
+            <Field
+              id="email"
+              name="email"
+              placeholder="Enter your email here..."
+              type="email"
+              className="w-full my-3 border border-gray-300 rounded p-3"
+            />
 
-          <label htmlFor="password">Password</label>
-          <Field
-            id="password"
-            name="password"
-            placeholder="Enter your password here..."
-            type="password"
-          />
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
+            <label htmlFor="password" className="w-full">
+              Password
+            </label>
+            <Field
+              id="password"
+              name="password"
+              placeholder="Enter your password here..."
+              type="password"
+              className="w-full my-3 border border-gray-300 rounded p-3"
+            />
+
+            <button
+              type="submit"
+              className="w-full text-center py-3 mt-3 rounded bg-sky-400 text-gray-50 shadow-md"
+            >
+              Submit
+            </button>
+          </Form>
+        </Formik>
+      </div>
+      <div className="text-center mt-6">
+        Don't have an account?{" "}
+        <Link to="/register" className="text-blue-500 border-blue-500 border-b">
+          Register
+        </Link>{" "}
+      </div>
     </div>
   );
 }
