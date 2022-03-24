3a2f65da67dfe1b23ad79c9e4ca7ce0f451ffe61
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2022-03-24 14:42:21 +1100

    added procfile for deploying app to heroku

--- server/index.js
+++ server/index.js
@@ -5,6 +5,6 @@ const config = require("./utils/config");
 
 const server = http.createServer(app);
 
-server.listen(config.PORT, () => {
+server.listen(config.PORT || 3001, () => {
   logger.debug(`Server running on port ${config.PORT}`);
 });
