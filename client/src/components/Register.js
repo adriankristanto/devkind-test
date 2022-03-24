93b2b6e230fc257c201b858eee88cac705cb4853
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2022-03-24 13:09:56 +1100

    added styling to the register component

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { Formik, Field, Form } from "formik";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function Register() {
   const { register, setCurrentUser, setToken } = useAuth();
@@ -23,46 +23,67 @@ export default function Register() {
   };
 
   return (
-    <div>
-      <h1>Register</h1>
-      <Formik
-        initialValues={{
-          name: "",
-          birthdate: "",
-          email: "",
-          password: "",
-        }}
-        onSubmit={handleSubmit}
-      >
-        <Form>
-          <label htmlFor="name">Name</label>
-          <Field id="name" name="name" placeholder="Enter your name here..." />
+    <div className="container max-w-sm flex-1 flex flex-col items-center justify-center">
+      <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+        <h1 className="mb-8 text-3xl text-center">Register</h1>
+        <Formik
+          initialValues={{
+            name: "",
+            birthdate: "",
+            email: "",
+            password: "",
+          }}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <label htmlFor="name">Name</label>
+            <Field
+              id="name"
+              name="name"
+              placeholder="Enter your name here..."
+              className="w-full my-3 border border-gray-300 rounded p-3"
+            />
 
-          <label htmlFor="birthdate">Birthdate</label>
-          <Field
-            id="birthdate"
-            name="birthdate"
-            placeholder="Enter your birthdate here (YYYY/MM/DD)..."
-          />
+            <label htmlFor="birthdate">Birthdate</label>
+            <Field
+              id="birthdate"
+              name="birthdate"
+              placeholder="Enter your birthdate here (YYYY/MM/DD)..."
+              className="w-full my-3 border border-gray-300 rounded p-3"
+            />
 
-          <label htmlFor="email">Email</label>
-          <Field
-            id="email"
-            name="email"
-            placeholder="Enter your email here..."
-            type="email"
-          />
+            <label htmlFor="email">Email</label>
+            <Field
+              id="email"
+              name="email"
+              placeholder="Enter your email here..."
+              type="email"
+              className="w-full my-3 border border-gray-300 rounded p-3"
+            />
 
-          <label htmlFor="password">Password</label>
-          <Field
-            id="password"
-            name="password"
-            placeholder="Create a new password here..."
-            type="password"
-          />
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
+            <label htmlFor="password">Password</label>
+            <Field
+              id="password"
+              name="password"
+              placeholder="Create a new password here..."
+              type="password"
+              className="w-full my-3 border border-gray-300 rounded p-3"
+            />
+            <button
+              type="submit"
+              className="w-full text-center py-3 mt-3 rounded bg-sky-400 text-gray-50 shadow-md"
+            >
+              Submit
+            </button>
+          </Form>
+        </Formik>
+      </div>
+      <div className="text-center mt-6">
+        Already have an account?{" "}
+        <Link to="/login" className="text-blue-500 border-blue-500 border-b">
+          Login
+        </Link>{" "}
+      </div>
     </div>
   );
 }
